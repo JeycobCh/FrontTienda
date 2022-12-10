884cd86bc1d9434e23ac5460c067d75a5a0094e0
Jeycob <jchavezc25@upao.edu.pe>
2022-12-10 01:49:41 -0500

    subir

--- src/app/models/producto.ts
+++ src/app/models/producto.ts
@@ -3,6 +3,7 @@ import { Categoria } from "./categoria";
 export class Producto {
     id!: number;
     nombre!: string;
+    descripcion!: string;
     precio!: number;
     imagen!: string;
     categoria!: Categoria;
